70ce5353ac950a1ddf109bb80389cfa81e9b6fbb
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2025-12-01 11:08:39 +0700

    Change Bulma CSS import to use style.css

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import Oruga from "@oruga-ui/oruga-next";
 import { bulmaConfig } from "@oruga-ui/theme-bulma";
 import App from "./App.vue";
 import router from "./router";
-import "@oruga-ui/theme-bulma/dist/bulma.css";
+import "@oruga-ui/theme-bulma/style.css";
 
 const app = createApp(App);
 
